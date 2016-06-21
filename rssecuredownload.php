4b316a67ffc7dbe3a1e8f4574ff31f5b703922a3
Rene <git-connect@rs-softweb.de>
2016-06-21 19:45:49 +0200

    Updated copyright to new year

--- rssecuredownload.php
+++ rssecuredownload.php
@@ -1,26 +1,31 @@
 <?php
-/***************************************************************
-*  Copyright notice
-*
-*  (c) 2008-2014 Rene <typo3@rs-softweb.de>
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+/**
+ *  Copyright notice
+ *
+ *  (c) 2008-2016 Rene <typo3@rs-softweb.de>
+ *  All rights reserved
+ *
+ *  You may not remove or change the name of the author above. See:
+ *  http://www.gnu.org/licenses/gpl-faq.html#IWantCredit
+ *
+ *  This script is part of the Typo3 project. The Typo3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  A copy is found in the textfile GPL.txt distributed with these scripts.
+ *
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ */
 
 session_start();
 $file = $_SESSION['tx_rssecuredownload_pi1']['file'];
