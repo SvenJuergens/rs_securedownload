dbdef600fe64ec0e1f437ccab7e03033554b739c
Rene <git-connect@rs-softweb.de>
2016-11-04 20:03:10 +0100

    Version 0.3.3 upload to TER

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "rs_securedownload".
  *
- * Auto generated 03-05-2015 00:25
+ * Auto generated 04-11-2016 19:48
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -14,10 +14,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Secure Download Form',
 	'description' => 'Display Downloads only with correct code',
 	'category' => 'plugin',
-	'shy' => 0,
+	'shy' => '',
 	'version' => '0.3.3',
-	'dependencies' => '',
-	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
 	'module' => 'mod1',
@@ -25,7 +23,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => 1,
 	'createDirs' => '',
 	'modify_tables' => '',
-	'clearcacheonload' => 0,
+	'clearcacheonload' => '',
 	'lockType' => '',
 	'author' => 'Rene',
 	'author_email' => 'typo3@rs-softweb.de',
@@ -42,9 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:31:{s:9:"ChangeLog";s:4:"d41d";s:21:"ext_conf_template.txt";s:4:"1a42";s:12:"ext_icon.gif";s:4:"88fe";s:17:"ext_localconf.php";s:4:"32e5";s:14:"ext_tables.php";s:4:"8aa8";s:14:"ext_tables.sql";s:4:"c855";s:19:"flexform_ds_pi1.xml";s:4:"21c7";s:34:"icon_tx_rssecuredownload_codes.gif";s:4:"88fe";s:13:"locallang.xml";s:4:"bb0e";s:16:"locallang_db.xml";s:4:"b8fb";s:20:"rssecuredownload.php";s:4:"8e43";s:7:"tca.php";s:4:"2a23";s:14:"doc/manual.sxw";s:4:"01ea";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"bd4e";s:14:"mod1/index.php";s:4:"a5a7";s:18:"mod1/locallang.xml";s:4:"4e35";s:22:"mod1/locallang_mod.xml";s:4:"7541";s:19:"mod1/moduleicon.gif";s:4:"88fe";s:14:"pi1/ce_wiz.gif";s:4:"44d4";s:37:"pi1/class.tx_rssecuredownload_pi1.php";s:4:"8681";s:45:"pi1/class.tx_rssecuredownload_pi1_wizicon.php";s:4:"3fe4";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"4090";s:16:"pi1/template.htm";s:4:"ebee";s:24:"pi1/static/editorcfg.txt";s:4:"cd12";s:20:"pi1/static/setup.txt";s:4:"6c69";s:14:"res/export.gif";s:4:"bd0e";s:15:"res/garbage.gif";s:4:"90c6";s:13:"res/minus.gif";s:4:"e4ff";s:12:"res/plus.gif";s:4:"cdba";}',
-	'suggests' => array(
-	),
+	'_md5_values_when_last_written' => 'a:55:{s:9:"ChangeLog";s:4:"d41d";s:21:"ext_conf_template.txt";s:4:"bc96";s:12:"ext_icon.gif";s:4:"88fe";s:17:"ext_localconf.php";s:4:"4378";s:14:"ext_tables.php";s:4:"02f2";s:14:"ext_tables.sql";s:4:"c855";s:19:"flexform_ds_pi1.xml";s:4:"ccda";s:34:"icon_tx_rssecuredownload_codes.gif";s:4:"88fe";s:13:"locallang.xml";s:4:"a6b1";s:16:"locallang_db.xml";s:4:"e45d";s:20:"rssecuredownload.php";s:4:"fcf7";s:7:"tca.php";s:4:"5900";s:26:"Documentation/Includes.txt";s:4:"a51e";s:23:"Documentation/Index.rst";s:4:"63e9";s:26:"Documentation/Settings.yml";s:4:"a7c5";s:39:"Documentation/Administration/Images.txt";s:4:"410d";s:38:"Documentation/Administration/Index.rst";s:4:"7128";s:45:"Documentation/Images/manual_html_18a25c21.png";s:4:"fdb4";s:45:"Documentation/Images/manual_html_22ebf792.gif";s:4:"1563";s:45:"Documentation/Images/manual_html_2d858b1d.png";s:4:"d88b";s:45:"Documentation/Images/manual_html_3c9c2593.png";s:4:"0eb5";s:45:"Documentation/Images/manual_html_424718f7.png";s:4:"8279";s:46:"Documentation/Images/manual_html_m1e1b9630.png";s:4:"0f37";s:46:"Documentation/Images/manual_html_m2b3d318a.png";s:4:"4622";s:46:"Documentation/Images/manual_html_m46ce57b7.png";s:4:"21cd";s:46:"Documentation/Images/manual_html_m51aa42d4.png";s:4:"5d07";s:45:"Documentation/Images/manual_html_mc843e4d.png";s:4:"8d5b";s:37:"Documentation/Introduction/Images.txt";s:4:"6c24";s:36:"Documentation/Introduction/Index.rst";s:4:"f7d6";s:37:"Documentation/KnownProblems/Index.rst";s:4:"24ca";s:32:"Documentation/TodoList/Index.rst";s:4:"da82";s:35:"Documentation/UsersManual/Index.rst";s:4:"9e2a";s:66:"Documentation/UsersManual/Backend-StatisticAndReporting/Images.txt";s:4:"1336";s:65:"Documentation/UsersManual/Backend-StatisticAndReporting/Index.rst";s:4:"7d5c";s:53:"Documentation/UsersManual/FrontendCreation/Images.txt";s:4:"e727";s:52:"Documentation/UsersManual/FrontendCreation/Index.rst";s:4:"0e44";s:14:"doc/manual.sxw";s:4:"01ea";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"5db8";s:14:"mod1/index.php";s:4:"3df0";s:18:"mod1/locallang.xml";s:4:"b81a";s:22:"mod1/locallang_mod.xml";s:4:"066f";s:19:"mod1/moduleicon.gif";s:4:"88fe";s:14:"pi1/ce_wiz.gif";s:4:"44d4";s:37:"pi1/class.tx_rssecuredownload_pi1.php";s:4:"e879";s:45:"pi1/class.tx_rssecuredownload_pi1_wizicon.php";s:4:"c1a1";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"8ccf";s:16:"pi1/template.htm";s:4:"9fa5";s:24:"pi1/static/editorcfg.txt";s:4:"9a36";s:20:"pi1/static/setup.txt";s:4:"9c73";s:14:"res/export.gif";s:4:"bd0e";s:15:"res/garbage.gif";s:4:"90c6";s:13:"res/minus.gif";s:4:"e4ff";s:12:"res/plus.gif";s:4:"cdba";}',
 );
 
 ?>
